57911807c1697158f0c1a6b4b820d166a7c6f4a2
Rasyid MM <rasyidmaulidmajid@gmail.com>
2020-02-22 15:23:57 +0700

    constant

---
+++ src/main/java/com/catdemo/demo/util/constants/SoalJenisConstant.java
@@ -0,0 +1,6 @@
+package com.catdemo.demo.util;
+
+public class Constans {
+    public static String SOALJENISGETALL  = "Get All soal Jenis Success";
+    public static String SOALJENISADDSUCCESS = "Add Soal Jenis Success";
+}
